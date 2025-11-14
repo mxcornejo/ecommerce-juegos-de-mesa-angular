33dc22a00f4e6898f442088f80514c8cbe754cdc
Manu Cornejo <48033239+mxcornejo@users.noreply.github.com>
2025-11-14 10:21:52 -0300

    feat: implementa producto segun data

--- src/app/pages/product/product.ts
+++ src/app/pages/product/product.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Product as ProductModel } from '../../models/product.interface';
+import { Category } from '../../models/category.interface';
+import { PRODUCTS, CATEGORIES } from '../../data/mock-data';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product.html',
   styleUrl: './product.scss',
 })
-export class Product {
+export class Product implements OnInit {
+  private route = inject(ActivatedRoute);
 
+  product: ProductModel | undefined;
+  category: Category | undefined;
+  readonly discountPercentage = 15;
+
+  ngOnInit(): void {
+    const productId = Number(this.route.snapshot.paramMap.get('id'));
+    this.product = PRODUCTS.find((p) => p.id === productId);
+
+    if (this.product) {
+      this.category = CATEGORIES.find((c) => c.id === this.product!.categoryId);
+    }
+  }
+
+  get discount(): number {
+    // Calcula un descuento del 15% para mostrar el precio original
+    return this.product ? Math.round(this.product.price / 0.85) : 0;
+  }
 }
