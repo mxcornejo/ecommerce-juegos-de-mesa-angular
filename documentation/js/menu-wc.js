07ad6c9a25153bd44584dc7bd03f8fd476a53b20
Manu Cornejo <48033239+mxcornejo@users.noreply.github.com>
2025-11-28 09:53:23 -0300

    feat: se agrega compodoc al proyecto

---
+++ documentation/js/menu-wc.js
@@ -0,0 +1,265 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">ecommerce-juegos-de-mesa-angular documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                                <li class="link">
+                                    <a href="overview.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-keypad"></span>Overview
+                                    </a>
+                                </li>
+
+                            <li class="link">
+                                <a href="index.html" data-type="chapter-link">
+                                    <span class="icon ion-ios-paper"></span>
+                                        README
+                                </a>
+                            </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+
+                    </ul>
+                </li>
+                    <li class="chapter modules">
+                        <a data-type="chapter-link" href="modules.html">
+                            <div class="menu-toggler linked" data-bs-toggle="collapse" ${ isNormalMode ?
+                                'data-bs-target="#modules-links"' : 'data-bs-target="#xs-modules-links"' }>
+                                <span class="icon ion-ios-archive"></span>
+                                <span class="link-name">Modules</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                        </a>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="modules-links"' : 'id="xs-modules-links"' }>
+                            <li class="link">
+                                <a href="modules/TemplatePlaygroundModule.html" data-type="entity-link" >TemplatePlaygroundModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ?
+                                            'data-bs-target="#components-links-module-TemplatePlaygroundModule-a48e698b66bad8be9ff3b78b5db8e15ee6bb54bd2575fdb1bb61a34e76437cc54b2e161854c3d6c97b4c751d05ff3a43b70b87ceffd46d3c5bf53f6f161e3044"' : 'data-bs-target="#xs-components-links-module-TemplatePlaygroundModule-a48e698b66bad8be9ff3b78b5db8e15ee6bb54bd2575fdb1bb61a34e76437cc54b2e161854c3d6c97b4c751d05ff3a43b70b87ceffd46d3c5bf53f6f161e3044"' }>
+                                            <span class="icon ion-md-cog"></span>
+                                            <span>Components</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-TemplatePlaygroundModule-a48e698b66bad8be9ff3b78b5db8e15ee6bb54bd2575fdb1bb61a34e76437cc54b2e161854c3d6c97b4c751d05ff3a43b70b87ceffd46d3c5bf53f6f161e3044"' :
+                                            'id="xs-components-links-module-TemplatePlaygroundModule-a48e698b66bad8be9ff3b78b5db8e15ee6bb54bd2575fdb1bb61a34e76437cc54b2e161854c3d6c97b4c751d05ff3a43b70b87ceffd46d3c5bf53f6f161e3044"' }>
+                                            <li class="link">
+                                                <a href="components/TemplatePlaygroundComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >TemplatePlaygroundComponent</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                <li class="chapter inner">
+                                    <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ?
+                                        'data-bs-target="#injectables-links-module-TemplatePlaygroundModule-a48e698b66bad8be9ff3b78b5db8e15ee6bb54bd2575fdb1bb61a34e76437cc54b2e161854c3d6c97b4c751d05ff3a43b70b87ceffd46d3c5bf53f6f161e3044"' : 'data-bs-target="#xs-injectables-links-module-TemplatePlaygroundModule-a48e698b66bad8be9ff3b78b5db8e15ee6bb54bd2575fdb1bb61a34e76437cc54b2e161854c3d6c97b4c751d05ff3a43b70b87ceffd46d3c5bf53f6f161e3044"' }>
+                                        <span class="icon ion-md-arrow-round-down"></span>
+                                        <span>Injectables</span>
+                                        <span class="icon ion-ios-arrow-down"></span>
+                                    </div>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="injectables-links-module-TemplatePlaygroundModule-a48e698b66bad8be9ff3b78b5db8e15ee6bb54bd2575fdb1bb61a34e76437cc54b2e161854c3d6c97b4c751d05ff3a43b70b87ceffd46d3c5bf53f6f161e3044"' :
+                                        'id="xs-injectables-links-module-TemplatePlaygroundModule-a48e698b66bad8be9ff3b78b5db8e15ee6bb54bd2575fdb1bb61a34e76437cc54b2e161854c3d6c97b4c751d05ff3a43b70b87ceffd46d3c5bf53f6f161e3044"' }>
+                                        <li class="link">
+                                            <a href="injectables/HbsRenderService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >HbsRenderService</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="injectables/TemplateEditorService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >TemplateEditorService</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="injectables/ZipExportService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >ZipExportService</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </li>
+                </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AdminDashboard.html" data-type="entity-link" >AdminDashboard</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AdminLogin.html" data-type="entity-link" >AdminLogin</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/App.html" data-type="entity-link" >App</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Banner.html" data-type="entity-link" >Banner</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CardCategory.html" data-type="entity-link" >CardCategory</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CardProduct.html" data-type="entity-link" >CardProduct</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Cart.html" data-type="entity-link" >Cart</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Categories.html" data-type="entity-link" >Categories</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Checkout.html" data-type="entity-link" >Checkout</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Confirmation.html" data-type="entity-link" >Confirmation</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/EditProfile.html" data-type="entity-link" >EditProfile</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Footer.html" data-type="entity-link" >Footer</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Hero.html" data-type="entity-link" >Hero</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Home.html" data-type="entity-link" >Home</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Navbar.html" data-type="entity-link" >Navbar</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Product.html" data-type="entity-link" >Product</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Products.html" data-type="entity-link" >Products</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Profile.html" data-type="entity-link" >Profile</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/RecoverPassword.html" data-type="entity-link" >RecoverPassword</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/SignIn.html" data-type="entity-link" >SignIn</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/SignUp.html" data-type="entity-link" >SignUp</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/AuthService.html" data-type="entity-link" >AuthService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/Cart.html" data-type="entity-link" >Cart</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/OrderService.html" data-type="entity-link" >OrderService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/CartItem.html" data-type="entity-link" >CartItem</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Category.html" data-type="entity-link" >Category</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/CompoDocConfig.html" data-type="entity-link" >CompoDocConfig</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/LoginCredentials.html" data-type="entity-link" >LoginCredentials</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Order.html" data-type="entity-link" >Order</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Product.html" data-type="entity-link" >Product</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/RegisterData.html" data-type="entity-link" >RegisterData</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Session.html" data-type="entity-link" >Session</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Template.html" data-type="entity-link" >Template</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/User.html" data-type="entity-link" >User</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+                        </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -3,6 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Cart as CartService } from '../../services/cart';
 
+/**
+ * Componente de página del carrito de compras.
+ * Muestra los productos agregados, permite modificar cantidades y eliminar items.
+ *
+ * @usageNotes
+ * - Envío gratis para compras sobre $50.000
+ * - Cantidad mínima por producto: 1
+ */
 @Component({
   selector: 'app-cart',
   imports: [CommonModule, RouterLink],
@@ -10,40 +18,103 @@ import { Cart as CartService } from '../../services/cart';
   styleUrl: './cart.scss',
 })
 export class Cart {
+  /** Servicio del carrito inyectado para gestionar productos */
   cartService = inject(CartService);
 
+  /**
+   * Obtiene los items del carrito.
+   *
+   * @returns Lista de productos en el carrito con cantidad
+   */
   get cartItems() {
     return this.cartService.items();
   }
 
+  /**
+   * Obtiene el total de unidades en el carrito.
+   *
+   * @returns Suma de todas las cantidades
+   */
   get totalItems() {
     return this.cartService.totalItems();
   }
 
+  /**
+   * Obtiene el subtotal del carrito.
+   *
+   * @returns Suma de (precio * cantidad) de cada producto
+   */
   get subtotal() {
     return this.cartService.subtotal();
   }
 
+  /**
+   * Obtiene el total a pagar.
+   *
+   * @returns Total incluyendo envío si aplica
+   */
   get total() {
     return this.cartService.total();
   }
 
+  /**
+   * Indica si el envío es gratis.
+   *
+   * @returns true si subtotal >= $50.000
+   */
   get shippingFree() {
     return this.subtotal >= 50000;
   }
 
+  /**
+   * Aumenta la cantidad de un producto en 1.
+   *
+   * @param productId ID del producto a modificar
+   * @param currentQuantity Cantidad actual del producto
+   *
+   * @example
+   * increaseQuantity(1, 2); // Cambia qty de 2 a 3
+   */
   increaseQuantity(productId: number, currentQuantity: number): void {
     this.cartService.updateQuantity(productId, currentQuantity + 1);
   }
 
+  /**
+   * Disminuye la cantidad de un producto en 1.
+   * Si llega a 0, el producto se elimina del carrito.
+   *
+   * @param productId ID del producto a modificar
+   * @param currentQuantity Cantidad actual del producto
+   *
+   * @example
+   * decreaseQuantity(1, 2); // Cambia qty de 2 a 1
+   */
   decreaseQuantity(productId: number, currentQuantity: number): void {
     this.cartService.updateQuantity(productId, currentQuantity - 1);
   }
 
+  /**
+   * Elimina un producto del carrito.
+   *
+   * @param productId ID del producto a eliminar
+   *
+   * @example
+   * removeItem(1); // Elimina producto con ID 1
+   */
   removeItem(productId: number): void {
     this.cartService.removeFromCart(productId);
   }
 
+  /**
+   * Calcula el total de un item (precio × cantidad).
+   *
+   * @param price Precio unitario del producto
+   * @param quantity Cantidad del producto
+   * @returns Total del item
+   *
+   * @example
+   * getItemTotal(10000, 3); // Retorna 30000
+   */
   getItemTotal(price: number, quantity: number): number {
     return price * quantity;
   }
