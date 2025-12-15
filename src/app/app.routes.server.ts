b4223004f3375b2b249a9e138e80d1ec1307c4b8
Manu Cornejo <48033239+mxcornejo@users.noreply.github.com>
2025-12-14 21:25:49 -0300

    feat: agregar rutas de productos con renderizado en cliente

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/:category',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'product/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
